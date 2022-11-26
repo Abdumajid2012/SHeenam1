2f46b6d5f3a4c12a96d821db7c189d1eb587332d
Abdumajid2012 <90825804+Abdumajid2012@users.noreply.github.com>
2022-11-26 20:45:41 +0500

    INFRA:  Initalize Unit Test Project

---
+++ Sheenam.Api.Tests.Unit/DeleteMe.cs
@@ -0,0 +1,21 @@
+﻿//==================================================
+// Copyright (c) Coalition Of Good-Hearted Engineers
+// Free To Use Comfort and Peace
+//==================================================
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Sheenam.Api.Tests.Unit
+{
+    public class DeleteMe
+    {
+        [Fact]
+        public void ShouldBeTrue() => Assert.True(true);
+
+    }
+}
