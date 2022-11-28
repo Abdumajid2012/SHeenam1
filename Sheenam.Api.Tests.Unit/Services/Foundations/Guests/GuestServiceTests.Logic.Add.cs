f4d49cc9c72d3161add054f03ff444f0f6dba97a
Abdumajid2012 <90825804+Abdumajid2012@users.noreply.github.com>
2022-11-28 16:45:17 +0500

    ShouldAddGuestAsync -> FAIL

---
+++ Sheenam.Api.Tests.Unit/Services/Foundations/Guests/GuestServiceTests.Logic.Add.cs
@@ -0,0 +1,67 @@
+﻿//==================================================
+// Copyright (c) Coalition Of Good-Hearted Engineers
+// Free To Use To Find Comfort and Peace
+//==================================================
+
+using FluentAssertions;
+using Moq;
+using Sheenam.Api.Models.Foundations.Guests;
+using Xunit;
+
+namespace Sheenam.Api.Tests.Unit.Services.Foundations.Guests
+{
+    public partial class GuestServiceTests
+    {
+
+        [Fact]
+        public async Task ShouldAddGuestInWrongWayAsync()
+        {
+            // Arrange
+            Guest randomGuest = new Guest
+            {
+                Id = Guid.NewGuid(),
+                FirstName = "Alex",
+                LastName = "Doe",
+                Address = "Brooks Str.#12",
+                DataOfBirth = new DateTimeOffset(),
+                Email = "rondom@mail.ru",
+                Gender = GenderType.Male,
+                PhoneNumber = "32123233"
+            };
+
+            this.storageBrokerMock.Setup(broker =>
+            broker.InsertGuestAsync(randomGuest))
+                .ReturnsAsync(randomGuest);
+            // Act
+            Guest actual = await this.guestService.AddGuestAsync(randomGuest);
+            // Assert
+            actual.Should().BeEquivalentTo(randomGuest);
+        }
+        [Fact]
+        public async Task ShouldAddGuestAsync()
+        {
+            // given
+            Guest randomGuest = CreateRandomGuest();
+            Guest inputGuest = randomGuest;
+            Guest returningGuest = inputGuest;
+            Guest expectedGuest = returningGuest;
+
+            this.storageBrokerMock.Setup(broker =>
+            broker.InsertGuestAsync(inputGuest))
+                .ReturnsAsync(returningGuest);
+
+            // when
+            Guest actualGuest =
+                await this.guestService.AddGuestAsync(inputGuest);
+
+            // then
+            actualGuest.Should().BeEquivalentTo(expectedGuest);
+
+            this.storageBrokerMock.Verify(broker =>
+            broker.InsertGuestAsync(inputGuest),
+            Times.Once);
+
+            this.storageBrokerMock.VerifyNoOtherCalls();
+        }
+    }
+}
